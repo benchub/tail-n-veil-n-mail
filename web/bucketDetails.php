86dd896d25380435bc5c7700b6ed80586d01c97c
Ben Chobot <bench@silentmedia.com>
2014-01-14 23:09:08 +0000

    Polish the php webapp turd a little more.

--- web/bucketDetails.php
+++ web/bucketDetails.php
@@ -1,27 +1,39 @@
+<html>
+  <head>
+    <style type="text/css">
+      tr:nth-child(even){
+        background-color:white;
+      }
+      tr:nth-child(odd){
+        background-color:Lavender;
+      }
+    </style>
+  </head>
+<body>
 <?php
 $days = 1;
 $bucket = " is null";
 
 if(isset($_GET["days"]))
 {
-        if(is_numeric($_GET["days"]))
-        {
-                if($_GET["days"] > 0)
-                {
-                        $days = $_GET["days"];
-                }
-        }
+	if(is_numeric($_GET["days"]))
+	{
+		if($_GET["days"] > 0)
+		{
+			$days = $_GET["days"];
+		}
+	}
 }
 
 if(isset($_GET["id"]))
 {
-        if(is_numeric($_GET["id"]))
-        {
-                if($_GET["id"] > 0)
-                {
-                        $bucket = " = " . $_GET["id"];
-                }
-        }
+	if(is_numeric($_GET["id"]))
+	{
+		if($_GET["id"] > 0)
+		{
+			$bucket = " = " . $_GET["id"];
+		}
+	}
 }
 
 // Connecting, selecting database
@@ -29,63 +41,80 @@ $dbconn = pg_connect("host=blahblah dbname=tnvnm user=www password=blahblah")
     or die('Could not connect: ' . pg_last_error());
 
 
+$query = "SELECT finished,host,event from events where finished > now()-interval '1 day'*$days and bucket_id $bucket order by finished desc limit 500";
 if (isset($_GET["n"]))
 {
-        $eventQuark = "normalize_query(event)";
+	$query = "SELECT count(*),max(finished),normalize_query(event) from events where finished > now()-interval '1 day'*$days and bucket_id $bucket group by normalize_query(event) order by count(*)  desc limit 500";
 }
-else
-{
-        $eventQuark = "event";
-}
-$query = "SELECT host,finished,$eventQuark from events where finished > now()-interval '1 day'*$days and bucket_id $bucket order by finished desc limit 500";
+
 $events = pg_query($query) or die('Query failed: ' . pg_last_error());
 
 echo "<h2>Showing the " . pg_num_rows($events) . " instances of ";
 
-if ($bucket == " is null")   
+if ($bucket == " is null")
 {
-        echo "interesting things";
+	echo "interesting things";
 }
 else
 {
-        $query = "SELECT name from buckets where id $bucket";
-        $bucket_info = pg_query($query) or die('Query failed: ' . pg_last_error());
-        while ($row = pg_fetch_row($bucket_info))
-        {
-                echo $row[0];
-        }
-        pg_free_result($bucket_info);
+	$query = "SELECT name from buckets where id $bucket";
+	$bucket_info = pg_query($query) or die('Query failed: ' . pg_last_error());
+	while ($row = pg_fetch_row($bucket_info))
+	{
+		echo $row[0];
+	}
+	pg_free_result($bucket_info);
 }
 
 echo " over the last $days days</h2>";
 
+echo "(back to the <a href='overview.php?days=$days'>overview)<p>";
+
 $me = "<a href='bucketDetails.php?days=$days";
-if ($bucket != " is null")   
+if ($bucket != " is null")
 {
-        $me = $me . "&id=" . $_GET["id"];
+	$me = $me . "&id=" . $_GET["id"];
 }
 
 if (isset($_GET["n"]))
 {
-        echo "[ $me'>Show queries as they were</a> | Showing queries normalized ]";
+	echo "[ $me'>Show queries as they were</a> | Showing queries normalized ]";
 }
 else
 {
-        echo "[ Showing queries as they were | $me&n=1'>Show queries normalized</a> ]";
+	echo "[ Showing queries as they were | $me&n=1'>Show queries normalized</a> ]";
 }
 
 echo "<hr><table>\n";
     echo "\t<tr>\n";
+if (isset($_GET["n"]))
+{
+        echo "\t\t<td>Count</td>\n";
+        echo "\t\t<td>Most Recent</td>\n";
+        echo "\t\t<td>What</td>\n";
+    echo "\t</tr>\n";
+  while ($row = pg_fetch_row($events))
+  {
+    echo "\t<tr>\n";
+	echo "\t\t<td>" . htmlentities($row[0], ENT_QUOTES) . "</td>\n";
+	echo "\t\t<td>" . htmlentities($row[1], ENT_QUOTES) . "</td>\n";
+	echo "\t\t<td><pre>" . $row[2] . "</pre></td>\n";
+    echo "\t</tr>\n";
+  }
+}
+else
+{
         echo "\t\t<td>When</td>\n";
         echo "\t\t<td>Where</td>\n";
         echo "\t\t<td>What</td>\n";
     echo "\t</tr>\n";
+}
 while ($row = pg_fetch_row($events))
 {
     echo "\t<tr>\n";
-        echo "\t\t<td>" . htmlentities($row[1], ENT_QUOTES) . "</td>\n";
-        echo "\t\t<td>" . htmlentities($row[0], ENT_QUOTES) . "</td>\n";
-        echo "\t\t<td><pre>" . $row[2] . "</pre></td>\n";
+	echo "\t\t<td>" . htmlentities($row[0], ENT_QUOTES) . "</td>\n";
+	echo "\t\t<td>" . htmlentities($row[1], ENT_QUOTES) . "</td>\n";
+	echo "\t\t<td><pre>" . $row[2] . "</pre></td>\n";
     echo "\t</tr>\n";
 }
 echo "</table><hr>\n";
@@ -96,3 +125,5 @@ pg_free_result($events);
 // Closing connection
 pg_close($dbconn);
 ?>
+</body>
+</html>
--- web/overview.php
+++ web/overview.php
@@ -3,15 +3,24 @@ $days = 1;
 
 if(isset($_GET["days"]))
 {
-        if(is_numeric($_GET["days"]))
-        {
-                if($_GET["days"] > 0)
-                {
-                        $days = $_GET["days"];
-                }
-        }
+	if(is_numeric($_GET["days"]))
+	{
+		if($_GET["days"] > 0)
+		{
+			$days = $_GET["days"];
+		}
+	}
 }
 
+
+// Connecting, selecting database
+$dbconn = pg_connect("host=blahblah dbname=tnvnm user=www password=blahblah")
+    or die('Could not connect: ' . pg_last_error());
+
+// how many days ago did our current sprint start?
+// '2013-12-28' is the day one sprint started, and they restart every 3 weeks
+$query = "SELECT extract(days from current_date-(select date '2013-12-28' + interval '3 weeks' * ((extract(epoch from current_date)-extract(epoch from date '2013-12-28'))::int / 1728000)))";
+$daysInSprint = pg_query($query) or die('Query failed: ' . pg_last_error());
 
 echo "<h2>Showing log counts over the last $days days</h2>\n";
 
@@ -19,13 +28,9 @@ echo "show instead: <pre>";
 echo "<a href='overview.php?days=.042'> 1 hour |</a>";
 echo "<a href='overview.php?days=1'> 1 day |</a>";
 echo "<a href='overview.php?days=7'> 1 week |</a>";
-echo "<a href='overview.php?days=21'> 1 sprint |</a>";
+echo "<a href='overview.php?days=", pg_fetch_result($daysInSprint,0,0), "'> this sprint |</a>";
 echo "<a href='overview.php?days=90'> 1 quarter</a>";
 echo "</pre>";
-
-// Connecting, selecting database
-$dbconn = pg_connect("host=blahblah dbname=tnvnm user=www password=blahblah")
-    or die('Could not connect: ' . pg_last_error());
 
 // Performing SQL query
 $query = "SELECT count(*),name,bucket_id from events,buckets where bucket_id=buckets.id and events.finished > now()-interval '1 day'*$days group by name,bucket_id order by name";
@@ -40,21 +45,21 @@ echo "<table>\n";
         echo "\t\t<td>Bucket</td>\n";
     echo "\t</tr>\n";
 while ($row = pg_fetch_row($namedThings))
-{   
+{
     echo "\t<tr>\n";
         echo "\t\t<td><a href='bucketDetails.php?days=$days&id=$row[2]'>$row[0]</a></td>\n";
-        if(preg_match('/^CNVS-\d+$/',$row[1]))
-        {
-                echo "\t\t<td><a href='https://instructure.atlassian.net/browse/$row[1]'>$row[1]</a></td>\n";
-        }
-        else
-        {
-                echo "\t\t<td>" . htmlentities($row[1], ENT_QUOTES) . "</td>\n";
-        }
+	if(preg_match('/^CNVS-\d+$/',$row[1]))
+	{
+	        echo "\t\t<td><a href='https://instructure.atlassian.net/browse/$row[1]'>$row[1]</a></td>\n";
+	}
+	else
+	{
+	        echo "\t\t<td>" . htmlentities($row[1], ENT_QUOTES) . "</td>\n";
+	}
     echo "\t</tr>\n";
 }
 while ($row = pg_fetch_row($interestingThings))
-{   
+{
     echo "\t<tr>\n";
         echo "\t\t<td><a href='bucketDetails.php?days=$days'>$row[0]</a></td>\n";
         echo "\t\t<td><b>interesting things</b></td>\n";
